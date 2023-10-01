c9b6558374c431c2beb22d2c181e0a1fd6c6331d
jiawei <guojw7@foxmail.com>
2023-10-01 14:36:18 -0700

    1. added some interfaces

--- frontend/src/api/pictures.js
+++ frontend/src/api/pictures.js
@@ -1,4 +1,6 @@
-import {get,post} from './http'
+import {get, post, put} from './http'
 
 export const getMyPictures=(pageParam)=>get('/pictures/',pageParam)
 export const uploadPictures=(form)=>get('/pictures/upload', form)
+export const likePicture=(pictureId)=>put('/pictures/like',pictureId)
+export const editPicture=(img)=>post('/picture/edit',img)
